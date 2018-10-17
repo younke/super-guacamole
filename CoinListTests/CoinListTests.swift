2f4c80011df15076e0bf66e96741da0665938652
younke <kewgram@gmail.com>
2018-10-17 17:26:09 +0900

    Initial Commit

---
+++ CoinListTests/CoinListTests.swift
@@ -0,0 +1,34 @@
+//
+//  CoinListTests.swift
+//  CoinListTests
+//
+//  Created by Vasily Ptitsyn on 17/10/2018.
+//  Copyright © 2018 younke. All rights reserved.
+//
+
+import XCTest
+@testable import CoinList
+
+class CoinListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
